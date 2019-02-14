6f9ae4db84ad2519dda9d84d6bcdf8ca45a5602e
Tolendi10 <47203652+Tolendi10@users.noreply.github.com>
2019-02-14 20:03:47 +0600

    Add files via upload

---
+++ Week 2/Task 2/Task 2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+            string t = Console.ReadLine();
+            System.IO.StreamReader file = new System.IO.StreamReader(t);
+            string line = file.ReadLine();
+            string[] arr = Regex.Split(line, " ");
+            int x;
+            foreach (string i in arr)
+            {
+                x = int.Parse(i);
+                for (int z = 2; z <= Math.Sqrt(x); ++z)
+                {
+                    if (x % z == 0)
+                    {
+                        x = 0;
+                        break;
+                    }
+                }
+                if (x > 1)
+                {
+                    s += i;
+                    s += " ";
+
+                }
+            }
+            file.Close();
+            string t2 = Console.ReadLine();
+            System.IO.File.WriteAllText(t2, s);
+        }
+    }
+}
